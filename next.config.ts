4c118abe421756f41e9a0677b0e050a2de1687e1
Oleksander <potebenkooleksandr00@gmail.com>
2025-06-18 20:20:13 +0300

    fix(ci): pnpm installed before caching dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  //TODO: Enable experimental features
+  // experimental: {
+  //   ppr: "incremental",
+  // },
 };
 
 export default nextConfig;
